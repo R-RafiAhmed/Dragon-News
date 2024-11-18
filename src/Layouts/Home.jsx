dee24e0a4f4a95ea3851a0d89d1a25fc282c7794
R-RafiAhmed <rafiahamed7706@gmail.com>
2024-11-18 19:40:11 +0600

    project setup done

---
+++ src/Layouts/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../Components/Header";
+
+
+const Home = () => {
+    return (
+        <>
+            <header>
+                <Header></Header>
+            </header>
+        </>
+    );
+};
+
+export default Home;
